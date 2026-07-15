f87191d96ecee0bebee51452dcdb5c4896076ada
obrilmentii-crypto <Obrilmentii@icloud.com>
2026-07-15 13:05:14 +0100

    feat:add foundational sprints metrics calculator engine

---
+++ project_tracker.py
@@ -0,0 +1,19 @@
+projectname ="Alpha portugal intergration"
+totaltask=32
+blockedtask=3
+avghourspertask=4.5
+completedtask=11
+print("hi")
+
+def calculatingmetric(): 
+  Netactivetask=totaltask-completedtask
+  Truecompletionrate=(completedtask/totaltask-blockedtask)*100
+  Totalhourexpended=(completedtask*avghourspertask)
+  Projecthoursremaining=(blockedtask*avghourspertask)
+
+  print("project:",projectname)
+  print("Netactivetask:",Netactivetask)
+  print("Truecompletionrate:",Truecompletionrate)
+  print("Totalhourexpended:",Totalhourexpended)
+  print("Projecthoursremaining:",Projecthoursremaining)
+  calculatingmetric()
