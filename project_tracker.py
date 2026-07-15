a92461e2d9574ee93be45a54936738424210af92
obrilmentii-crypto <Obrilmentii@icloud.com>
2026-07-15 16:32:56 +0100

    fix function indentation error

--- project_tracker.py
+++ project_tracker.py
@@ -3,17 +3,18 @@ totaltask=32
 blockedtask=3
 avghourspertask=4.5
 completedtask=11
-print("hi")
 
 def calculatingmetric(): 
-  Netactivetask=totaltask-completedtask
-  Truecompletionrate=(completedtask/totaltask-blockedtask)*100
-  Totalhourexpended=(completedtask*avghourspertask)
-  Projecthoursremaining=(blockedtask*avghourspertask)
+    Netactivetask=totaltask-completedtask
+    Truecompletionrate=(completedtask/totaltask-blockedtask)*100
+    Totalhourexpended=(completedtask*avghourspertask)
+    Projecthoursremaining=(blockedtask*avghourspertask)
 
-  print("project:",projectname)
-  print("Netactivetask:",Netactivetask)
-  print("Truecompletionrate:",Truecompletionrate)
-  print("Totalhourexpended:",Totalhourexpended)
-  print("Projecthoursremaining:",Projecthoursremaining)
-  calculatingmetric()
+    print("project:",projectname)
+    print("Netactivetask:",Netactivetask)
+    print("Truecompletionrate:",Truecompletionrate)
+    print("Totalhourexpended:",Totalhourexpended)
+    print("Projecthoursremaining:",Projecthoursremaining)
+    
+    from datetime import datetime; print("DEBUG: System runtime timestamp:", datetime.now().isoformat())
+calculatingmetric()
